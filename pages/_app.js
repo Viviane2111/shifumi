4935b9714479f99025b749cabe15b59a3c495315
Viviane <arrghe.dev@gmail.com>
2024-06-14 05:37:47 +0200

    ajout d'une icone meta

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,9 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Next.js App</title>
+        {/* <link rel="icon" href="/favicon.ico" sizes="16x16"/> */}
+        <link rel="icon" href="/PRV.png" sizes="16x16" type="image/png"/>
+        <title>ShiFuMi</title>
       </Head>
       <Component {...pageProps} />
     </>
